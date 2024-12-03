8ea59eb783d32bff3437ff996f7be8d851ea4237
Elian Ferreira <57501971+elianferreira@users.noreply.github.com>
2024-12-03 20:28:25 -0300

    Adicionado upload de imagem

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 let concatenatedText = ""; // String para armazenar o texto concatenado
 
-// Adicionar evento de clique em cada imagem
+// Adicionar evento de clique em cada imagem de Braille
 document.querySelectorAll('.braille-key').forEach(image => {
     image.addEventListener('click', async (event) => {
         const imageName = event.target.dataset.image; // Nome da imagem clicada
@@ -34,3 +34,48 @@ document.querySelectorAll('.braille-key').forEach(image => {
     });
 });
 
+// Função para exibir a imagem selecionada do upload
+const imageUploadInput = document.getElementById('image-upload');
+const imagePreview = document.getElementById('image-preview');
+
+imageUploadInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+// Adicionar evento para enviar a imagem via upload
+document.getElementById('image-upload').addEventListener('change', async (event) => {
+    const resultDiv = document.getElementById('result');
+    const formData = new FormData();
+    formData.append('file', event.target.files[0]); // Adiciona o arquivo ao FormData
+
+    try {
+        // Enviar requisição para o endpoint de upload de imagem
+        const response = await fetch('http://127.0.0.1:5000/predict_via_upload', { 
+            method: 'POST',
+            body: formData, // Envia o arquivo
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            // Concatenar a letra retornada ao texto
+            concatenatedText += result.prediction;
+
+            // Atualizar a exibição do texto concatenado
+            resultDiv.textContent = concatenatedText;
+        } else {
+            resultDiv.textContent = `Erro: ${result.error}`;
+        }
+    } catch (error) {
+        console.error('Erro ao conectar com o servidor:', error);
+        resultDiv.textContent = "Erro ao conectar com o servidor.";
+    }
+});
