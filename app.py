5b46bda525671001f71a3f05abb4bbccbb09cc1a
Elian Ferreira <57501971+elianferreira@users.noreply.github.com>
2024-12-03 20:27:13 -0300

    Update app.py
    Adicionado upload de imagem

--- app.py
+++ app.py
@@ -29,29 +29,21 @@ class_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Função para pré-processar a imagem
-def process_image(image_path):
+def process_image(image):
     """
-    Pré-processa a imagem a partir de um caminho:
+    Pré-processa a imagem:
     - Redimensiona para 32x32
     - Converte para escala de cinza
     - Normaliza os valores (0 a 1)
     """
-    image = Image.open(image_path).resize((32, 32)).convert('L')
+    image = image.resize((32, 32)).convert('L')
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=(0, -1))
     return image
 
-# Página inicial para exibir as imagens
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# Endpoint para predição
+# Endpoint para predição com nome da imagem
 @app.route('/predict', methods=['POST'])
 def predict():
-    """
-    Endpoint para fazer predições com base na imagem referenciada.
-    """
     try:
         data = request.json  # Recebe o nome da imagem
         if not data or 'image' not in data:
@@ -59,7 +51,34 @@ def predict():
 
         image_name = data['image']
         image_path = os.path.join(base_dir, "static", "braille", image_name)  # Caminho corrigido
-        processed_image = process_image(image_path)
+        image = Image.open(image_path)
+        processed_image = process_image(image)
+
+        # Faz a predição
+        predictions = model.predict(processed_image)
+        predicted_class = class_names[np.argmax(predictions)]
+
+        return jsonify({'prediction': predicted_class})
+    except Exception as e:
+        return jsonify({'error': f'Ocorreu um erro ao processar a imagem: {str(e)}'}), 500
+
+
+# Novo endpoint para predição com upload de imagem
+@app.route('/predict_via_upload', methods=['POST'])
+def predict_via_upload():
+    try:
+        # Verifique se um arquivo foi enviado
+        if 'file' not in request.files:
+            return jsonify({'error': 'Nenhum arquivo foi enviado.'}), 400
+
+        file = request.files['file']
+        
+        if file.filename == '':
+            return jsonify({'error': 'Nenhum arquivo selecionado.'}), 400
+        
+        # Abrir a imagem
+        image = Image.open(file)
+        processed_image = process_image(image)
 
         # Faz a predição
         predictions = model.predict(processed_image)
@@ -72,4 +91,3 @@ def predict():
 # Executar a API
 if __name__ == '__main__':
     app.run(debug=True)
-
